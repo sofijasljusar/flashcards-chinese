437386e0526117611fe2c3a51baf871baee5e264
sofijasljusar <sofija.sljusar@gmail.com>
2025-01-20 18:34:22 +0200

    Add background image and sound

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pandas
 import random
 from tkinter import messagebox
+from pygame import mixer
 
 # Constants
 BACKGROUND_COLOR = "#004A65"
@@ -19,6 +20,7 @@ def show_next_card():
         if timer is not None:
             window.after_cancel(timer)
         all_words_learnt()
+        window.quit()
     else:
         if timer is not None:
             window.after_cancel(timer)
@@ -71,38 +73,42 @@ else:
 # Window set up
 window = Tk()
 window.title("Learn Chinese with Flashcards!")
-window.config(bg=BACKGROUND_COLOR, pady=100, padx=200)
-
-# Background
 screen_height = window.winfo_screenheight()
 screen_width = window.winfo_screenwidth()
+window.minsize(width=screen_width, height=screen_height)
 
-# TODO: 1. Add background image and make the window full screen
-# TODO: 2. Handle end of words with some animation
-# background = Canvas(width=screen_width, height=screen_height, highlightthickness=0)
-# # background_image = PhotoImage(file="...")
-# background.grid(row=0, column=0, columnspan=2, rowspan=2)
+# Background
+# TODO: 2. Handle end of words situation with some animation
+background = Canvas(window, width=1728, height=1117, highlightthickness=0)
+background_image = PhotoImage(file="images/beach1.png")
+background.create_image(0, 0, image=background_image, anchor=NW)
+background.place(x=0, y=0)
 
 # Flashcard
-canvas = Canvas(width=824, height=550, highlightthickness=0, bg=BACKGROUND_COLOR)
-flashcard_front = PhotoImage(file="images/white_card.png")
-flashcard_back = PhotoImage(file="images/black_card.png")
-card_image = canvas.create_image(412, 275)
+canvas = Canvas(width=800, height=526, highlightthickness=0)
+flashcard_front = PhotoImage(file="images/white_square.png")
+flashcard_back = PhotoImage(file="images/black_square.png")
+card_image = canvas.create_image(400, 263)
 card_language = canvas.create_text(400, 115, font=(FONT_NAME, 40, "italic"))
 card_transcription = canvas.create_text(400, 175, font=(FONT_NAME, 30, "normal"))
-card_word = canvas.create_text(400, 263, font=(FONT_NAME, 80, "bold"))
-canvas.grid(row=0, column=0, columnspan=2)
+card_word = canvas.create_text(400, 263, font=(FONT_NAME, 80, "bold"), width=800)
+canvas.place(x=464, y=150)
 
 # Correct button
 correct_icon = PhotoImage(file="images/correct_button.png")
 correct_button = Button(image=correct_icon, width=95, height=95, highlightthickness=0, command=remove_learnt_card)
-correct_button.grid(row=1, column=1)
+correct_button.place(x=564, y=750)
 
 # Wrong button
 wrong_icon = PhotoImage(file="images/wrong_button.png")
 wrong_button = Button(image=wrong_icon, width=95, height=95, highlightthickness=0, command=show_next_card)
-wrong_button.grid(row=1, column=0)
+wrong_button.place(x=1064, y=750)
 
+# ---------------------------------- PLAY SOUND ---------------------------------- #
+
+mixer.init()
+sound = mixer.Sound("sounds/ocean_waves.mp3")
+sound.play(loops=-1)
 show_next_card()
 
 window.mainloop()
